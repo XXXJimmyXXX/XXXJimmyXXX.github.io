38845d368d4ee3a243f0525f68a62303be634729
XXXJimmyXXX <web@ym-digital-lab-c6.local>
2015-06-24 14:09:03 -0400

    To do list

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) { 
  $scope.message = 'leave a random comment here.';
+  $scope.todos = [
+    'Learn Sketch', 
+    'Look at Dribbble and feel inferior',
+    'Actually learn how to use the Pen tool'
+  ];
+  
+  $scope.done = function(todo) {
+    var indexOf = $scope.todos.indexOf(todo);
+    if (indexOf !== -1) {
+      $scope.todos.splice(indexOf, 1);
+    }
+  };
+  
  });
