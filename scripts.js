ba73d14c9087d73023ec76fca83999a0696a788a
XXXJimmyXXX <web@ym-digital-lab-c6.local>
2015-06-25 13:47:30 -0400

    Local Storage

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
+var hasStorage;
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="result"></div>
+
+<script>
+// Check browser support
+if (typeof(Storage) != "undefined") {
+   hasStorage=true;
+    // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+hasStorage=false
+}
+</script>
+
+</body>
+</html>
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) { 
  $scope.message = 'leave a random comment here.';
@@ -11,12 +30,21 @@ app.controller('TodoCtrl', function($scope) {
     var indexOf = $scope.todos.indexOf(todo);
     if (indexOf !== -1) {
       $scope.todos.splice(indexOf, 1);
+      if(hasStorage){
+     // Store
+    localStorage.setItem("todos", JSON.stringify($scope.todos));
     }
+    }
+    
   };
   $scope.add = function(e) {
     if (e.which && e.which === 13) {
       $scope.todos.push($scope.newTodo);
       $scope.newTodo = '';
+       if(hasStorage){
+     // Store
+    localStorage.setItem("todos", JSON.stringify($scope.todos));
+    }
     }
   };
   
